7a40cad440f56abebb76b7bc1ee7d30ead13b43a
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-02-28 22:38:00 +0530

    Update Week 6 Assignment in Python

--- Assignment6.py
+++ Assignment6.py
@@ -1,23 +1,21 @@
 def solve():
-    n, p, f = input().split(" ")
-    n = int(n)
-    p = int(p)
-    f = int(f)
-    morning = list(input().split(" "))
-    evening = list(input().split(" "))
-    #  print(morning, evening)
-    #  morning = [int(x) for x in morning]
-    for i in range(len(morning)):
-        morning[i] = int(morning[i])
-        evening[i] = int(evening[i])
-    #  evening = [int(x) for x in evening]
-    morning.sort(); evening.sort()
-    #  print(morning)
-    #  print(evening)
-    count = 0
-    for i in range(len(morning)):
-        if morning[i] + evening[len(evening) - i - 1] > p:
-            count += morning[i] + evening[len(evening) - i - 1] - p
-    print(count * f)
+	n, p, f = input().split(" ")
+	n = int(n)
+	p = int(p)
+	f = int(f)
+	mo = input().rstrip()
+	morning = list(mo.split(" "))
+	morning = [int(x) for x in morning]
+	# print(morning)
+	ev = input().rstrip()
+	evening = list(ev.split(" "))
+	evening = [int(x) for x in evening]
+	# print(evening)
+	morning.sort(); evening.sort(reverse = True)
+	count = 0
+	for i in range(len(morning)):
+		if morning[i] + evening[i] > p:
+			count += morning[i] + evening[i] - p
+	print(count * f)
 
 solve()
