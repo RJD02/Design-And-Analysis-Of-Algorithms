ba6244cdf096c075e54ca6569e3e18722e5c5263
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-03-06 23:16:49 +0530

    Add useful comments

--- bruteCommonSubword.cpp
+++ bruteCommonSubword.cpp
@@ -10,23 +10,23 @@ pair<string, int> match(string s1, string s2) {
 	string res = "", curr_str = "";
 	for(int i = 0; i < s1.length(); i++) {
 		for(int ii = 0; ii < s2.length(); ii++) {
-			if(s1[i] == s2[ii]) {
+			if(s1[i] == s2[ii]) { // Starting position of our common substring.
 				curr_count = 0;
 				int start_s1 = i;
 				int start_s2 = ii;
 				curr_str = "";
 				while(start_s2 < s2.length() && start_s1 < s1.length()) {
-					if(s1[start_s1] != s2[start_s2])
+					if(s1[start_s1] != s2[start_s2]) // Our common substring ends here.
 						break;
 					curr_count++;
-					curr_str += s1[start_s1];
+					curr_str += s1[start_s1]; // Add character by character.
 					start_s1++;
 					start_s2++;
 				}
 			}
 			max_count = max(curr_count, max_count);
-			// res = max(res, curr_str);
-			if(res.length() < curr_str.length())
+			// res = max(res, curr_str); // Could have been nice if I could do this, like in python
+			if(res.length() < curr_str.length()) // Prepare for our longest common substring to arrive.
 				res = curr_str;
 		}
 	}
